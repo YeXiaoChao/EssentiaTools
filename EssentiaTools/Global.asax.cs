55e22bfc41492f5bcb2e22f2f2c9f0b537e2bd81
YeXiaoChao <yechao@ycdoit.com>
2016-03-08 12:00:29 +0800

    create

---
+++ EssentiaTools/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using EssentiaTools.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EssentiaTools
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            //通过这里添加的语句，能让 Ninject 来创建 MVC 框架所需的任何对象实例，
+            //这便将 DI 放到了这一示例应用程序中
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ EssentiaTools/Models/Discount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentiaTools.Models
+{
+    public interface IDiscountHelper
+    {
+        //将一个折扣运用于一个十进制的值
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal discountSize;
+
+        public DefaultDiscountHelper(decimal discountParam)
+        {
+            discountSize = discountParam;
+        }
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (discountSize / 100m * totalParam));
+        }
+    }
+}
