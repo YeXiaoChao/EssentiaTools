8340cd3a6a8ca7b054b37dd80e2109329ea03755
YeXiaoChao <yechao@ycdoit.com>
2016-03-14 11:29:43 +0800

    update

---
+++ EssentiaTools.Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentiaTools.Models;
+
+namespace EssentiaTools.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IDiscountHelper getTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
+
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            //准备
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+
+            //动作
+            var discountedTotal = target.ApplyDiscount(total);
+
+            //断言
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+
+        [TestMethod]
+        public void Discount_Between_10_And_100()
+        {
+            //准备
+            IDiscountHelper target = getTestObject();
+
+            //动作
+            decimal TenDollarDiscount = target.ApplyDiscount(10);
+            decimal HundredDollarDiscount = target.ApplyDiscount(100);
+            decimal FiftyDollarDiscount = target.ApplyDiscount(50);
+
+            //断言
+            Assert.AreEqual(5, TenDollarDiscount, "$10 discount is wrong");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 discoutn is wrong");
+            Assert.AreEqual(45,FiftyDollarDiscount, "$50 discount is wrong");
+        }
+
+        [TestMethod]
+        public void Discount_Less_Than_10()
+        {
+            IDiscountHelper target = getTestObject();
+
+            decimal discount5 = target.ApplyDiscount(5);
+            decimal discount0 = target.ApplyDiscount(0);
+
+            Assert.AreEqual(5, discount5);
+            Assert.AreEqual(0, discount0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Discount_Negative_Total()
+        {
+            IDiscountHelper target = getTestObject();
+
+            target.ApplyDiscount(-1);
+        }
+    }
+}
